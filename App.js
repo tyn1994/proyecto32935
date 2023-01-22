92066b8bc502b84df767201ebee6cb5529029154
tyn1994 <torrezmartin.developer@gmail.com>
2023-01-22 13:40:46 -0300

    Clase 4 OK

--- App.js
+++ App.js
@@ -1,11 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  FlatList,
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedTask, setSelectedTask] = useState(null);
+
+  const onHandlerChange = (text) => {
+    setTask(text);
+  };
+
+  const onHandlerSubmit = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: Math.random().toString,
+        value: task,
+      },
+    ]);
+
+    setTask("");
+  };
+
+  const onHandlerModal = (item) => {
+    setIsModalVisible(!isModalVisible);
+    setSelectedTask(item);
+  };
+
+  //console.warn("tasks", tasks);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.itemContainer}
+      onPress={() => onHandlerModal(item)}
+    >
+      <Text style={styles.itemList}>{item.value}</Text>
+    </TouchableOpacity>
+  );
+
+  const keyExtractor = (item) => item.id;
+
+  const onHandlerCancel = () => {
+    setIsModalVisible(!isModalVisible);
+    setSelectedTask(null);
+  };
+
+  const onHandlerDelete = () => {
+    setTasks((prevTaskList) =>
+      prevTaskList.filter((task) => task.id !== selectedTask.id)
+    );
+    setIsModalVisible(!isModalVisible);
+    setSelectedTask(null);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a new task"
+          autoComplete="off"
+          autoCorrect={false}
+          autoCapitalize="none"
+          value={task}
+          onChangeText={onHandlerChange}
+        />
+        <Button
+          disabled={!task}
+          title="Add"
+          color="#626893"
+          onPress={onHandlerSubmit}
+        />
+      </View>
+      <FlatList
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        style={styles.listContainer}
+      />
+      <Modal visible={isModalVisible} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Task Detail</Text>
+          <View style={styles.modalDetailContainer}>
+            <Text style={styles.modalDetailMessage}>
+              Are you sure to delete this item?
+            </Text>
+            <Text style={styles.selectedTask}>{selectedTask?.value}</Text>
+          </View>
+          <View style={styles.modalButtonContainer}>
+            <Button title="Cancel" color="#626893" onPress={onHandlerCancel} />
+            <Button title="Delete" color="red" onPress={onHandlerDelete} />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -13,8 +112,70 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  inputContainer: {
+    marginTop: 50,
+    marginHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  input: {
+    width: "75%",
+    borderBottomColor: "#000",
+    borderBottomWidth: 1,
+    height: 40,
+    color: "#626893",
+  },
+  listContainer: {
+    marginHorizontal: 20,
+    marginTop: 40,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    backgroundColor: "#626893",
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  itemList: {
+    fontSize: 14,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  modalContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+    paddingVertical: 20,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  modalDetailContainer: {
+    paddingVertical: 20,
+  },
+  modalDetailMessage: {
+    fontSize: 14,
+    color: "#212121",
+  },
+  selectedTask: {
+    fontSize: 14,
+    color: "#212121",
+    fontWeight: "bold",
+    paddingVertical: 10,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  modalButtonContainer: {
+    width: "70%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginHorizontal: 20,
   },
 });
